b8181c8bea9b87341740e533f63d4b76b18fd6e7
lndojaUWO <lndoja@uwo.ca>
2024-03-17 21:18:05 -0400

    commit

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,10 @@
 #include <MSE2202_Lib.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_TCS34725.h>
 #include <Wire.h>
+#include <ESP32Servo.h>
+
 
 #define LEFT_MOTOR_A        35                                                 // GPIO35 pin 28 (J35) Motor 1 A
 #define LEFT_MOTOR_B        36                                                 // GPIO36 pin 29 (J36) Motor 1 B
@@ -11,298 +14,260 @@
 #define RIGHT_MOTOR_B       38                                                 // GPIO38 pin 31 (J38) Motor 2 B
 #define ENCODER_LEFT_A      15                                                 // left encoder A signal is connected to pin 8 GPIO15 (J15)
 #define ENCODER_LEFT_B      16                                                 // left encoder B signal is connected to pin 8 GPIO16 (J16)
-#define ENCODER_RIGHT_A     11                                                 // right encoder A signal is connected to pin 19 GPIO11 (J11)
-#define ENCODER_RIGHT_B     12                                                 // right encoder B signal is connected to pin 20 GPIO12 (J12)
 #define MODE_BUTTON         0                                                  // GPIO0  pin 27 for Push Button 1
 #define MOTOR_ENABLE_SWITCH 3                                                  // DIP Switch S1-1 pulls Digital pin D3 to ground when on, connected to pin 15 GPIO3 (J3)
 #define POT_R1              1                                                  // when DIP Switch S1-3 is on, Analog AD0 (pin 39) GPIO1 is connected to Poteniometer R1
 #define SMART_LED           21                                                 // when DIP Switch S1-4 is on, Smart LED is connected to pin 23 GPIO21 (J21)
 #define SMART_LED_COUNT     1                                                  // number of SMART LEDs in use
+// set to false if using a common cathode LED
+#define commonAnode true
+#define SWITCH_PIN          46
 
-// all distances are written in cm
-#define TURN90DISTANCE      12                                                 
-#define PLOW_WIDTH          15                                                 
+unsigned int robotModeIndex = 0;                                              // robot operational state 
+unsigned int debounceTimer;         
 
-#define X_MAX               30
-#define Y_MAX               80
+unsigned int previousMillisColour;
+unsigned int currentMillisColour;
+unsigned int previousMillisServo;
+unsigned int currentMillisServo;
 
-#define KP                  30
-#define KI                  0
-#define KD                  0   
-int integral = 0;        
-float derivative = 0;
-float lastError = 0;
-
-
-unsigned int debounceTimer;                                                   
-unsigned long previousMillisGyro;                                                  
-unsigned long currentMillisGyro;                                                   
 
 const int PWM_Resolution = 8;                                                  // bit resolution
 const int MIN_PWM = 150;                                                       
 const int MAX_PWM = pow(2, PWM_Resolution) - 1;    
 const int PULSES_PER_REVOLUTION = 1096;                                        // encoder pulses per motor revolution
-unsigned char leftDriveSpeed = 0;                                                  
-unsigned char rightDriveSpeed = 0;                                                 
-float targetSpeed;
+                                           
+byte gammatable[256];
+float red, green, blue; // for color sensor
 
-const int WHEEL_DIAMETER = 5;
-unsigned int  robotModeIndex = 0;                                              // robot operational state 
-enum DriveStage { STOP, DRIVE_TO_END, TURN_LEFT, NEXT_COLUMN,TURN_RIGHT };     // enums that are used so i can use text names in the switch case instead of integers
-/*
-This array represents the actions the robot will take in one loop. 
-*/
-DriveStage driveLoops[9] = {STOP, DRIVE_TO_END, TURN_LEFT, NEXT_COLUMN, TURN_LEFT, DRIVE_TO_END, TURN_RIGHT, NEXT_COLUMN, TURN_RIGHT};
-int driveLoopIndex = 0; // which stage in the driveLoop we are at
-bool stageComplete;     // if True, start the next stage in the loop
-int numLoops = 0;     
+const int servoPin          = 41;                 // GPIO pin for servo motor
 
 Adafruit_NeoPixel smartLED = Adafruit_NeoPixel(SMART_LED_COUNT, SMART_LED, NEO_GRB + NEO_KHZ800);  
 
 // Motor, encoder, and IR objects (classes defined in MSE2202_Lib)
 Motion Bot = Motion();                                                         
-Encoders LeftEncoder = Encoders();                                             
-Encoders RightEncoder = Encoders();         
+Encoders LeftEncoder = Encoders();  
 
-// MPU6050 IMU Sensor
-Adafruit_MPU6050 mpu;                                                          // I2C address 0x68
-float angle = 0;
-bool measureAngle = false;
-float yAcceleration = 0;
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+Servo sorterServo;
+int servoPos = 0;
+
+long targetPos = 0;
+
+char result = 'w';
+
+bool servoToggle = false;
+
+bool roboMode = false;
 
 void setup() {
    smartLED.begin();                                                           
    smartLED.show(); // Initialize all pixels to 'off'
+
+   pinMode(SWITCH_PIN, INPUT_PULLUP);
+
+   sorterServo.attach(servoPin);
+
+   Wire.begin(47,48);
+
+   if (tcs.begin()) {
+   //Serial.println("Found sensor");
+   } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+   }
    
   // Set up motors and encoders
    Bot.driveBegin("D1", LEFT_MOTOR_A, LEFT_MOTOR_B, RIGHT_MOTOR_A, RIGHT_MOTOR_B); 
    LeftEncoder.Begin(ENCODER_LEFT_A, ENCODER_LEFT_B, &Bot.iLeftMotorRunning ); 
-   RightEncoder.Begin(ENCODER_RIGHT_A, ENCODER_RIGHT_B, &Bot.iRightMotorRunning ); 
    pinMode(MOTOR_ENABLE_SWITCH, INPUT_PULLUP);
    pinMode(MODE_BUTTON, INPUT_PULLUP);
 
-  Wire.begin(47,48);
-   // connect to mpu
-   if (!mpu.begin()) {
-      Serial.println("Failed to find MPU6050 chip");
-      while (1) {
-         delay(10);
-      }
-   }
-
-   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
 
    debounceTimer = 0;
-   //numLoops = numberOfLoops(X_MAX, PLOW_WIDTH);
-   numLoops = 1;
-   stageComplete = false;
+   // thanks PhilB for this gamma table!
+   // it helps convert RGB colors to what humans see
+   for (int i=0; i<256; i++) {
+      float x = i;
+      x /= 255;
+      x = pow(x, 2.5);
+      x *= 255;
 
-   previousMillisGyro = millis();
+      if (commonAnode) {
+      gammatable[i] = 255 - x;
+      } else {
+      gammatable[i] = x;
+      }
+      //Serial.println(gammatable[i]);
+   }
+
+   previousMillisColour = millis();
+   previousMillisServo = millis();
 }
 
 void loop() {
    long motorPosition[] = {0, 0}; // represented in encouder counts
    int potentiometer = 0; // raw ADC value from potentiometer
 
+   digitalRead(SWITCH_PIN) == HIGH ? roboMode = true : roboMode = false;
+
+
+
    noInterrupts();                                                             
    LeftEncoder.getEncoderRawCount();
-   RightEncoder.getEncoderRawCount();
    motorPosition[0] = LeftEncoder.lRawEncoderCount;                                                 
-   motorPosition[1] = RightEncoder.lRawEncoderCount;
    interrupts();                                                               
 
    buttonDebounce();
-   readMPU();
 
-  
-
-
-   if (robotModeIndex == 0 or driveLoopIndex == 9){
-         Bot.Stop("D1");
-         LeftEncoder.clearEncoder();                                        
-         RightEncoder.clearEncoder();
-         // set leds to red
-         setLedColor(255, 0, 0);
-         //numLoops = numberOfLoops(X_MAX, PLOW_WIDTH);
-         numLoops = 1;
-         driveLoopIndex = 0;
-         robotModeIndex = 0;
+   if (robotModeIndex == 0){
+   
+      Bot.Stop("D1");
+      LeftEncoder.clearEncoder(); 
+      Serial.println("Robot Stopped");                                       
+      // set leds to red
+      setLedColor(100, 100, 100);
+      robotModeIndex = 0;
    }
    else{
-      potentiometer = analogRead(POT_R1);
-      targetSpeed = map(potentiometer, 0, 4095, 0, 100);
-      
+      // set led to purple
+      setLedColor(100, 0, 100);
 
+      if (roboMode == true){
+         // potentiometer = analogRead(POT_R1); // read potentiometer value
+         // targetPos = map(potentiometer, 0, 4095, 0, 100); // map potentiometer value to motor position
+         // if (targetPos > 100){
+         //    Bot.Forward("D1", toPWM(100), toPWM(100));
+         // }
+         // else{
+         //    Bot.Stop("D1");
+         // }
+         Bot.Forward("D1", toPWM(100), toPWM(100));
 
-      /*
-      This switch case determines what the robot will do in each stage
-      for reference one loop is: 
-
-      STOP, DRIVE_TO_END, TURN_LEFT, NEXT_COLUMN, TURN_LEFT, DRIVE_TO_END, TURN_RIGHT, NEXT_COLUMN, TURN_RIGHT
-      
-      */
-      switch(driveLoops[driveLoopIndex]) {
-         case STOP:
-            Bot.Stop("D1");
-            LeftEncoder.clearEncoder();                                        
-            RightEncoder.clearEncoder();
-            // set leds to red
-            setLedColor(255, 0, 0);
-            stageComplete = true;
-            break;
-         case DRIVE_TO_END:
-            // set leds to green
-            setLedColor(0, 255, 0);
-            if (driveTo(Y_MAX, motorPosition[0])){
-                  stageComplete = true;                  
-            }
-            break;
-         case TURN_LEFT:
-            // set leds to blue
-            setLedColor(0, 0, 255);
-            if (turnTo(90, false)){
-                  stageComplete = true;
-            }
-            break;
-         case NEXT_COLUMN:
-            // set leds to yellow
-            setLedColor(255, 255, 0);
-            if (driveTo(PLOW_WIDTH, motorPosition[0])){
-                  stageComplete = true;
-            }
-            break;
-         case TURN_RIGHT:
-            // set leds to blue
-            setLedColor(0, 0, 255);
-            if (turnTo(-90, true)){
-                  stageComplete = true;
-            }
-            break;
       }
-      if (stageComplete){
-         driveLoopIndex = nextStage(); // finds out what stage the robot must do next
-      }
-   }
+      else{
+         if (readColor()){
+            // set leds to color
+            result = maxColor();
+            if (result == 'g'){
+               turnServo(0);
+
+            }
+            else{
+               turnServo(1);
+            }
+         }        
+      }      
+   }  
+      
 }
 
-void straightDriveSpeeds(){
 
-   float error = angle;
-   (error < 0.25 and error > -0.25) ? integral = 0 : integral += error;
-   derivative = error - lastError;
-   lastError = error;
+bool readColor(){
 
-   float correction = (KP * error + KI * integral + KD * derivative) * -1;
-   leftDriveSpeed = targetSpeed + correction;
-   rightDriveSpeed = targetSpeed - correction;
+   currentMillisColour = millis();
+   if (currentMillisColour - previousMillisColour > 60) {
 
-   if (leftDriveSpeed > 100){
-      rightDriveSpeed = rightDriveSpeed - (leftDriveSpeed - 100);
-      leftDriveSpeed = 100;
+      tcs.setInterrupt(false);  // turn on LED
+
+
+      tcs.getRGB(&red, &green, &blue);
+
+      tcs.setInterrupt(true);  // turn off LED
+      previousMillisColour = currentMillisColour;
+
+      // Serial.print("R:\t"); Serial.print(int(red)); 
+      // Serial.print("\tG:\t"); Serial.print(int(green)); 
+      // Serial.println("\tB:\t"); Serial.print(int(blue));
+      return true;
    }
-   if (rightDriveSpeed > 100){
-      leftDriveSpeed = leftDriveSpeed - (rightDriveSpeed - 100);
-      rightDriveSpeed = 100;
-   }
-   if (leftDriveSpeed < 0){
-      rightDriveSpeed = rightDriveSpeed + leftDriveSpeed -25;
-      leftDriveSpeed = 25;
-   }
-   if (rightDriveSpeed < 0){
-      leftDriveSpeed = leftDriveSpeed + rightDriveSpeed -25;
-      rightDriveSpeed = 25;
-   }
+   return false;
+
 }
 
+
+void driveTo(long distance, long motorPosition){
+   if(motorPosition < distance){ 
+      Bot.Forward("D1", toPWM(100), 0);  
+   }
+   if (motorPosition > distance){
+      Bot.Reverse("D1", toPWM(100), 0);
+   }
+}
 
 unsigned char toPWM(float speed){
    unsigned char result = map(speed, 0, 100, MIN_PWM, MAX_PWM);
    return (result);
 }
+   
+void turnServo(int choice){
+   currentMillisServo = millis();
+   if (servoToggle == true){
+      previousMillisServo = millis();
+      servoToggle = false;
+   }
+   switch (choice){
+      case 0:
+         if (currentMillisServo - previousMillisServo > 3000){
+            sorterServo.write(0);
+            servoToggle = true;
+         }
+         else{
+            sorterServo.write(45);
+         }
+         break;
+      case 1:
+         if (currentMillisServo - previousMillisServo > 3000){
+            sorterServo.write(90);
+            servoToggle = true;
+         }
+         else{
+            sorterServo.write(45);
+         }
+         break;
+   }
 
-void readMPU(){
-   sensors_event_t a , g, temp;
-   mpu.getEvent(&a, &g, &temp);
 
-   yAcceleration = a.acceleration.y;   
 
-   float angularVelocity = g.gyro.z;
-   angularVelocity += 0.0048; // offset
-   currentMillisGyro = millis();
-   float changeInAngle = angularVelocity * (currentMillisGyro - previousMillisGyro) / 1000;
-   changeInAngle = RAD_TO_DEG * changeInAngle;
-   measureAngle == true ? angle += changeInAngle : angle = 0;
-   previousMillisGyro = currentMillisGyro;
+
+
 }
 
-bool turnTo(int setAngle, bool turningRight){
-   if(abs(angle) < abs(setAngle)){
-      measureAngle = true;
-      turningRight == true ? Bot.Left("D1", toPWM(targetSpeed), toPWM(targetSpeed)) : Bot.Right("D1", toPWM(targetSpeed), toPWM(targetSpeed)); // misleading, bot.left turns it to the right
-      return false;
+
+char maxColor(){
+   if (red > green && red > blue){
+      red = 255;
+      green = 0;
+      blue = 0;
+      return('r');
+   }
+   else if (green > red && green > blue){
+      red = 0;
+      green = 255;
+      blue = 0;
+      return('g');
+   }
+   else if (blue > red && blue > green){
+      red = 0;
+      green = 0;
+      blue = 255;
+      return('b');
    }
    else{
-      angle = 0;
-      measureAngle = false;
-      Bot.Stop("D1");
-      LeftEncoder.clearEncoder();
-      RightEncoder.clearEncoder();
-      return true;
-   }
-}
+      red = 255;
+      green = 255;
+      blue = 255;
+      return('w');
+   }  
 
-// returns the number of encoder counts needed to drive a certain distance
-long getCM(long inCM){ 
-   return(inCM * PULSES_PER_REVOLUTION / (WHEEL_DIAMETER * 3.14159));
-}
-
-// input a distance in cm and it will drive forward that amount
-bool driveTo(int distance, long motorPosition){
-
-   straightDriveSpeeds();
-
-   if(motorPosition < getCM(distance)){ 
-      measureAngle = true;
-      Bot.Forward("D1", toPWM(leftDriveSpeed), toPWM(rightDriveSpeed));  
-      return false;
-   }
-   else{
-      measureAngle = false;
-      Bot.Stop("D1");
-      LeftEncoder.clearEncoder();
-      RightEncoder.clearEncoder();
-      return true;
-   }
 }
 
 void setLedColor(int r, int g, int b){
-   if (smartLED.getPixelColor(0) != smartLED.Color(r, g, b)){
-      smartLED.setPixelColor(0, smartLED.Color(r, g, b));
+   if (smartLED.getPixelColor(0) != smartLED.Color(g, r, b)){
+      smartLED.setPixelColor(0, smartLED.Color(g, r, b));
       smartLED.show();
    }
-}
-
-int numberOfLoops(int xMax, int plowWidth){
-   return xMax / plowWidth;
-}
-
-int nextStage(){
-   if(stageComplete){
-      stageComplete = false;
-      driveLoopIndex++;
-      if (driveLoopIndex == 9){ // if we have finished the loop
-         if (numLoops == 0){
-            return 9; // if we have done all the loops we return 9 to indicate that we are done
-         }
-         else{
-            numLoops--;
-            driveLoopIndex = 0;
-         }
-      }
-   }
-   return driveLoopIndex;
 }
 
 // placed into the depths so we dont have to look at it
@@ -334,6 +299,4 @@ void buttonDebounce(){
       }
    }
 }
-
-
 
